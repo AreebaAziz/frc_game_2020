a1fd86916bfcf46add8d71d6b89de68f10542282
Areeba Aziz <aziza11@mcmaster.ca>
2019-12-04 16:08:06 -0500

    added navigating thru pages support for leaderboard

--- maceng/leaderboard.py
+++ maceng/leaderboard.py
@@ -98,6 +98,36 @@ class Leaderboard(object):
 			BUTTONS[new_index].active = True 
 			BUTTONS[original_index].active = False
 
+	def _inc_active_page(self):
+		original_index = PAGES.index(self.active_page)
+		new_index = original_index + 1
+		if (new_index < len(PAGES)):
+			self.active_page = PAGES[new_index]
+			PAGES[new_index].active = True 
+			PAGES[original_index].active = False
+		if (new_index == len(PAGES)-1):
+			# on the last page, disable the NEXT button
+			NEXT_BTN.disabled = True 
+			self._dec_active_btn()
+		if (original_index == 0):
+			# if we just moved from the first page, enable the PREV button
+			PREV_BTN.disabled = False 
+
+	def _dec_active_page(self):
+		original_index = PAGES.index(self.active_page)
+		new_index = original_index - 1
+		if (new_index >= 0):
+			self.active_page = PAGES[new_index]
+			PAGES[new_index].active = True 
+			PAGES[original_index].active = False
+		if (new_index == 0):
+			# on the first page, disable the PREV button
+			PREV_BTN.disabled = True 
+			self._inc_active_btn()
+		if (original_index == len(PAGES)-1):
+			# if we just moved from the last page, enable the NEXT button
+			NEXT_BTN.disabled = False 		
+
 	def update(self, key_press):
 		if (key_press == K_RIGHT):
 			logging.debug("right key pressed")
@@ -109,6 +139,10 @@ class Leaderboard(object):
 			logging.debug("return key pressed on menu button \"{}\"".format(self.active_btn.label))
 			if (self.active_btn == MAIN_MENU):
 				return True 
+			elif (self.active_btn == NEXT_BTN):
+				self._inc_active_page()
+			elif (self.active_btn == PREV_BTN):
+				self._dec_active_page()
 		return False 
 
 BUTTONS = [
@@ -118,6 +152,8 @@ BUTTONS = [
 	Button("Next", 590, 560),
 ]
 MAIN_MENU = BUTTONS[0]
+PREV_BTN = BUTTONS[2]
+NEXT_BTN = BUTTONS[3]
 
 # pages 
 PAGES = [
