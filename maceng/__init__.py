9afe8747db7abd47551545294ecb95b7c7b76289
Areeba Aziz <aziza11@mcmaster.ca>
2020-01-10 15:35:35 -0500

    committed by Areeba

--- maceng/__init__.py
+++ maceng/__init__.py
@@ -2,7 +2,7 @@ import logging
 import os 
 import django 
 
-logging.basicConfig(format='[%(asctime)s] %(levelname)s: %(message)s', level=logging.DEBUG)
+logging.basicConfig(format='[%(asctime)s] %(levelname)s: %(message)s', level=logging.INFO)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'maceng.frc_game_2020.settings')
 django.setup()
