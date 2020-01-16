8c242b9a62061e775b06e031e9ce8def747c1a4b
Areeba Aziz <aziza11@mcmaster.ca>
2020-01-16 16:25:51 -0500

    changed admin site to view user data

--- maceng/backend/admin.py
+++ maceng/backend/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import User, Score
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'team', 'email')
+    readonly_fields = ('username', 'team', 'email')
+
+class ScoreAdmin(admin.ModelAdmin):
+	list_display = ('score', 'user', 'datetime')
+	readonly_fields = ('score', 'user', 'datetime')
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Score, ScoreAdmin)
