b06e4bcca976e247e75d14e636cf0cb0af753f81
Areeba Aziz <aziza11@mcmaster.ca>
2020-02-12 16:15:31 -0500

    export data into csv

--- maceng/backend/admin.py
+++ maceng/backend/admin.py
@@ -1,13 +1,46 @@
+import csv
 from django.contrib import admin
+from django.http import HttpResponse
 from .models import User, Score
 
-class UserAdmin(admin.ModelAdmin):
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
+
+class UserAdmin(admin.ModelAdmin, ExportCsvMixin):
     list_display = ('username', 'team', 'email')
     readonly_fields = ('username', 'team', 'email')
+    actions = ['export_as_csv']
 
-class ScoreAdmin(admin.ModelAdmin):
-	list_display = ('score', 'user', 'datetime')
-	readonly_fields = ('score', 'user', 'datetime')
+class ScoreAdmin(admin.ModelAdmin, ExportCsvMixin):
+    list_display = ('score', 'user', 'get_user_email', 'get_user_team', 'datetime')
+    list_filter = ('datetime', )
+    readonly_fields = ('score', 'user', 'datetime')
+    actions = ["export_as_csv"]
+
+    def get_user_email(self, obj):
+        return obj.user.email
+    get_user_email.short_description = "User Email"
+    get_user_email.admin_order_field = "user__email"
+
+    def get_user_team(self, obj):
+        return obj.user.team
+    get_user_team.short_description = "User Team"
+    get_user_team.admin_order_field = "user__team"
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Score, ScoreAdmin)
