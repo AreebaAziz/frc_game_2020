767eca683b913f35d2dc36e9fbfa133090380c85
Areeba Aziz <aziza11@mcmaster.ca>
2020-03-24 21:55:42 -0400

    committed by Areeba

--- maceng/receiver.py
+++ maceng/receiver.py
@@ -2,7 +2,7 @@ import logging
 from .models import Score
 
 def gameover(form_data):
-	logging.info("Gameover signal received. Form data: \n{}".format(form_data))
+	logging.debug("Gameover signal received. Form data: \n{}".format(form_data))
 	Score.add_score(
 		username=form_data["username"], 
 		score=form_data["score"],
